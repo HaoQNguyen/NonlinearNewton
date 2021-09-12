6241ecf9e459d0f0a29ffe04df39604c3156679a
HaoQNguyen <hao.q.nguyen.2000@gmail.com>
2021-09-12 00:03:51 -0400

    add autodiff  implementation

--- function.jl
+++ function.jl
@@ -64,3 +64,71 @@ function newton_raphson(x, dx, k, imax, tol)
     return x;
 
 end
+
+# --------------- Fucntions related to autodiff implementation ---------------
+
+f1(x, L, W, LT) = x[1] + x[2] + x[3] + x[4] + 4 * L + 3 * W - LT;
+f2(x, ki, k) = ki[1] * x[1] + k[1] * x[1]^3 - (ki[2] * x[2] + k[2] * x[2]^3);
+f3(x, ki, k) = ki[2] * x[2] + k[2] * x[2]^3 - (ki[3] * x[3] + k[3] * x[3]^3);
+f4(x, ki, k) = ki[3] * x[3] + k[3] * x[3]^3 - (ki[4] * x[4] + k[4] * x[4]^3);
+
+function f_autodiff(x, ki, k, L, W, LT)
+    return [f1(x, L, W, LT);
+            f2(x, ki, k);
+            f3(x, ki, k);
+            f4(x, ki, k)];
+end
+
+function jacobian_autodiff!(x, ki, k, L, W, LT)
+
+    # sparse vector from the output tuple from gradient()
+    (j1,) = gradient(x -> f1(x, L, W, LT), x);
+    (j2,) = gradient(x -> f2(x, ki, k), x);
+    (j3,) = gradient(x -> f3(x, ki, k), x);
+    (j4,) = gradient(x -> f4(x, ki, k), x);
+
+    # transpose vecors into column matrix
+    j1 = transpose(j1);
+    j2 = transpose(j2);
+    j3 = transpose(j3);
+    j4 = transpose(j4);
+
+    # concatenate arrays horizontally
+    return [j1; j2; j3; j4];
+
+end
+
+function newton_raphson_autodiff(x, ki, imax, tol)
+
+    L = 1.2;
+    W = 0.25;
+    LT = 25;
+
+    res = zeros(imax);
+    con = zeros(imax);
+    i = 1;
+    res[i] = norm(f_autodiff(x, ki, k, L, W, LT), Inf);
+    J = zeros(length(x), length(x));
+    J = jacobian_autodiff!(x, ki, k, L, W, LT);
+
+    i = 2;
+    while i < imax
+
+        xold = copy(x);
+        J = jacobian_autodiff!(x, ki, k, L, W, LT);
+        x = xold - inv(J) * f_autodiff(xold, ki, k, L, W, LT)
+
+        res[i] = norm(f_autodiff(x, ki, k, L, W, LT), Inf);
+        con[i] = abs(norm(f_autodiff(x, ki, k, L, W, LT), Inf) - norm(f_autodiff(xold, ki, k, L, W, LT), Inf));
+
+        if res[i] < tol && con[i] < tol
+            break;
+        end
+
+        i += 1;
+
+    end
+
+    return x;
+
+end
--- main.jl
+++ main.jl
@@ -1,4 +1,5 @@
 using LinearAlgebra;
+using Zygote;
 include("function.jl");
 
 x = ones(4,1);
@@ -6,10 +7,25 @@ dx = 1e-05;
 tol = 1e-05;
 imax = 100;
 
+println(" ----- Implement finite difference -----\n");
+
 k = [0.15, 0.25, 0.35, 0.45];
 x = newton_raphson(x, dx, k, imax, tol);
 display(x);
 
 k = [0, 0, 0, 0];
 x = newton_raphson(x, dx, k, imax, tol);
+display(x);
+
+println("\n ---------- Implement autodiff ----------\n");
+
+x = ones(4,1);
+ki = [1.5, 2.5, 3.5, 4.5];
+
+k = [0.15, 0.25, 0.35, 0.45];
+x = newton_raphson_autodiff(x, ki, imax, tol);
+display(x);
+
+k = [0, 0, 0, 0];
+x = newton_raphson_autodiff(x, ki, imax, tol);
 display(x);
